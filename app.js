3daa8a7f184a5c8f8cf9c26733313bedf291c231
Rohan Sharma <rhnsharma5113@gmail.com>
2017-12-28 20:51:18 +0530

    made some changes

--- app.js
+++ app.js
@@ -14,9 +14,7 @@ console.log('Starting app.js');
       var note =  notes.addNote(argv.title,argv.body);
       if (note) {
     console.log('Note Created');
-    console.log('--------');
-    console.log(`Title: ${note.title}`);
-    console.log(`Body : ${note.body}`);
+      notes.logNote(note);
       }
       else {
         console.log('Note title taken');
@@ -24,9 +22,18 @@ console.log('Starting app.js');
     }else if (command === 'list') {
         notes.getAll();
 }else if (command === 'read') {
-  notes.getNote(argv.title);
+  var note = notes.getNote(argv.title);
+  if (note) {
+    console.log('Note found');
+    notes.logNote(note);
+  }
+  else {
+    console.log('Note not found');
+  }
 }else if (command === 'remove') {
-  notes.removeNote(argv.title);
+  var noteRemoved =  notes.removeNote(argv.title);
+var message = noteRemoved ? 'Note was removed' : 'Note not found';
+console.log(message);
 }else {
   console.log('command not recognized');
 }
