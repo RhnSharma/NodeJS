5bde66d7352b5c7a176413c3edd2f42c6d94a580
Rohan Sharma <rhnsharma5113@gmail.com>
2017-12-31 23:34:43 +0530

    advanced yargs arguments update

--- app.js
+++ app.js
@@ -2,7 +2,30 @@
     const _ = require('lodash');
     const yargs = require('yargs');
     const notes = require('./notes.js');
-    const  argv = yargs.argv;
+    const titleOptions = {
+      title: 'Title of note',
+      demand:true,
+      alias: 't'
+    };
+    const bodyOptions = {
+      describe: 'Body of the note',
+      demand: true,
+      alias: 'b'
+    };
+    const  argv = yargs
+    .command('add','Add a new note',{
+    title: titleOptions,
+    body: bodyOptions
+    })
+    .command('list','List all notes')
+    .command('read','Read a note',{
+      title: titleOptions
+    })
+    .command('remove','Remove a note',{
+      title:titleOptions
+    })
+    .help()
+    .argv;
 
     var command = argv._[0];
 
@@ -10,13 +33,13 @@
       var note =  notes.addNote(argv.title,argv.body);
       if (note) {
     console.log('Note Created');
-      notes.logNote(note);
+    notes.logNote(note);
       }
       else {
         console.log('Note title taken');
       }
     }else if (command === 'list') {
-        var allNotes = notes.getAll();
+    var allNotes = notes.getAll();
     console.log(`Printing ${allNotes.length} note(s).`);
     allNotes.forEach((note) => notes.logNote(note));
 }else if (command === 'read') {
@@ -29,10 +52,9 @@
     console.log('Note not found');
   }
 }else if (command === 'remove') {
-  var noteRemoved =  notes.removeNote(argv.title);
+var noteRemoved =  notes.removeNote(argv.title);
 var message = noteRemoved ? 'Note was removed' : 'Note not found';
 console.log(message);
 }else {
   console.log('command not recognized');
 }
-
